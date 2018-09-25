6d90d51a6501815666bfacf7ec8243616a10bf4a
Danilo Vale <danilo.vale@xpi.com.br>
2018-09-25 19:23:45 -0300

    Adicionado o react-redux

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ const store = createStore(
     reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
+
   ReactDOM.render(
-    <App store={store} />,
     <Provider store={store}>
       <App />
     </Provider>,
